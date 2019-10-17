0f97d9581541c88717d0cb5cb282851470cb1a0c
MilkshakeForReal <38997672+MilkshakeForReal@users.noreply.github.com>
2019-10-17 14:59:06 -0400

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -4,7 +4,6 @@ Created on Thu Oct 17 20:48:30 2019
 
 @author: lenovo
 """
-
 import os
 import pickle
 import zipfile
@@ -18,7 +17,7 @@ class HARDataset(object):
                  data_path = './data',
                  store_path = './proc',
                  Gamma = 50,
-                 sell_loop = True,
+                 self_loop = True,
                  from_raw = False
             ):
         
@@ -27,10 +26,10 @@ class HARDataset(object):
         self.from_raw = from_raw
         self.Gamma = 50
         self.file_name = '/dataset-har-PUC-Rio-ugulino'
-        if not os.path.exists(self.data_path+self.file_name+ 'csv'):
-            archive = zipfile.ZipFile(self.data_path+self.file_name+'.zip')
-            archive.extractall(self.data_path)
-            archive.close()
+        #if not os.path.exists(self.data_path+self.file_name+ 'csv'):
+        #    archive = zipfile.ZipFile(self.data_path+self.file_name+'.zip')
+        #    archive.extractall(self.data_path)
+        #    archive.close()
         self.njoints = 4
         self.spa_edges = list(set([(i,j) for i in range(self.njoints) \
                           for j in range(self.njoints)]))
@@ -38,8 +37,7 @@ class HARDataset(object):
         self.tem_edges = [(i,i) for i in range(self.njoints)]
         self.pro = 0
         self._load()
-    def frames_to_stgraph(self, frames):
-        print(f"Processing stgraph {self.pro}...")
+    def frames_to_stgraph(self, frames, user):
         frame_graphs = {}
         frame_graphs[('fm_0', 'connect', 'fm_0')] = self.spa_edges
         for frame in range(1, frames.shape[0]):
@@ -49,23 +47,21 @@ class HARDataset(object):
             
         stgraph =  dgl.heterograph(frame_graphs)
         
-        xyz = frames[['x_1', 'y1', 'z1', 
+        xyz = frames[['x1', 'y1', 'z1', 
                       'x2', 'y2', 'z2',
                       'x3', 'y3', 'z3',
                       'x4', 'y4', 'z4']].values
         for frame in range(frames.shape[0]):
-            #x,y,z data
-            print(f"Processing frame {frame}...")
-            stgraph.nodes[f'fm_{frame}'].data = xyz[frame,:].reshape(self.njoints,
-                         -1)
+            #x,y,z feat
+            stgraph.nodes[f'fm_{frame}'].data['h'] = th.tensor(xyz[frame,:].reshape(self.njoints,-1))
         
         
         graph_feats = ['gender', 'age', 'how_tall_in_meters',
                        'weight', 'body_mass_index']  
         stgraph.gdata = {f:frames[f].values[0] for f in graph_feats}
-        self.pro += 1
-        print(stgraph)
-        return stgraph, frames.iloc[0,-1]
+        self.pro = self.pro + 1
+        stgraph.gdata['user'] =user
+        return stgraph
     def _load(self):
         if not self.from_raw:
             with open(self.store_path+'/stgraphs.pkl',  "rb") as f:
@@ -81,9 +77,16 @@ class HARDataset(object):
             har['how_tall_in_meters'] = har['how_tall_in_meters'].map(lambda x: float(x.replace(',','.')))
             har['body_mass_index'] = har['body_mass_index'].map(lambda x: float(x.replace(',','.')))
             
-            #
-            self.stgraphs, self.labels = har.groupby(['user','class']).apply(self.frames_to_stgraph)
-    
+            users = har['user'].unique()
+            classes = har['class'].unique()
+            grouped = har.groupby(['user','class'])
+            self.labels = []
+            for u in users:
+                for c in classes:
+                    print(f"Processing '{u}', '{c}'...")
+                    self.stgraphs = self.frames_to_stgraph(grouped.get_group((u, c)), u)
+                    self.labels.append(c)
+            print("Loaded!")
         with open(self.store_path+'/stgraphs.pkl', "wb") as f:
                 pickle.dump(self.stgraphs, f)
         with open(self.store_path+'labels.pkl', "wb") as f:
